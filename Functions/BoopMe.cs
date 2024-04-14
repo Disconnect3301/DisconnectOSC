75713cd40d77b1e2bbfc1284bb7907963ddf03bb
Disconnect3301 <disc0nnect@list.ru>
2024-04-14 12:51:00 +0300

    TryCatchErrorsWhenUsingAvatarConfig

--- Functions/BoopMe.cs
+++ Functions/BoopMe.cs
@@ -21,19 +21,44 @@ namespace MainOSC
         }
         public static async void Start()
         {
-            var config = OscAvatarConfig.Create("avtr_97c252b5-48f7-4435-a16f-da1448711953")!;
-
-            while (true)
+            try
             {
-                if (config.Parameters["Boop"] is bool && (bool)config.Parameters["Boop"])
+                var config = OscAvatarConfig.Create("avtr_97c252b5-48f7-4435-a16f-da1448711953")!;
+
+                while (true)
                 {
-                    OscChatbox.SendMessage("Boop! >.<", direct: true, complete: true);
-                    await Task.Delay(1000);
-                    OscChatbox.SendMessage("", direct: true, complete: false);
+#pragma warning disable CS8605
+                    if (config.Parameters["Boop"] is bool && (bool)config.Parameters["Boop"])
+                    {
+                        OscChatbox.SendMessage("Boop! >.<", direct: true, complete: true);
+                        await Task.Delay(1000);
+                        OscChatbox.SendMessage("", direct: true, complete: false);
+                    }
+#pragma warning restore CS8605
+                    await Task.Delay(10);
                 }
-                await Task.Delay(10);
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("[ERROR] In BoopMe.Start: " + e.Message);
+                Console.ResetColor();
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("[ERROR] In BoopMe.Start: " + e.Message);
+                Console.ResetColor();
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("[ERROR] In BoopMe.Start: " + e.Message);
+                Console.WriteLine(e.StackTrace);
+                Console.ResetColor();
             }
 
         }
+
     }
 }
